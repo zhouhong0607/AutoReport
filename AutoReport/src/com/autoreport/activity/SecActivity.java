bb35941735a792abd54b9371b9f6c039b8d94af7
KOSS <278111013@qq.com>
2016-03-08 13:40:55 +0800

    V1.0
    格式修改

--- AutoReport/src/com/autoreport/activity/SecActivity.java
+++ AutoReport/src/com/autoreport/activity/SecActivity.java
@@ -1,27 +1,6 @@
 package com.autoreport.activity;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
 import com.autoreport.app.R;
 import com.autoreport.database.DatabaseOperator;
@@ -32,20 +11,16 @@ import com.autoreport.datastructure.AutoreportApp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.ColorStateList;
+
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
+
 import android.text.TextPaint;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ForegroundColorSpan;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class SecActivity extends Activity
 {
@@ -112,32 +87,30 @@ public class SecActivity extends Activity
 		mtextview2.append("内存占用率:  " + info.getMemRate() + "\n");
 		mtextview2.append("CPU使用率:  " + info.getCpuRate() + "\n");
 
-		// 无线环境信息
-
-		mtextview3.append("无线环境信息:");
-
-		mtextview4.append("运营商:  " + info.getCorporation() + "\n");
-
-		mtextview4.append("LAC_GSM:  " + info.getLAC_GSM() + "\n");
-		mtextview4.append("Cell-ID_GSM:  " + info.getCell_Id_GSM() + "\n");
-
-		// mtextview4.append("RSSI: " + info.getRSSI() + "\n");
-
 		// 应用业务信息
 
-		mtextview5.append("应用业务信息:");
+		mtextview3.append("应用业务信息:");
 
-		mtextview6.append("启动时间:  " + info.getLaunTime() + "\n");
-		mtextview6.append("异常时间:  " + info.getExcepTime() + "\n");
-		mtextview6.append("上报时间:  " + info.getUploadTime() + "\n");
-		mtextview6.append("上报次数:  " + info.getUploadNum() + "\n");
-		mtextview6.append("退出时间:  " + info.getExitTime() + "\n");
-		mtextview6.append("运行时间:  " + info.getUseTime() + "\n");
-		mtextview6.append("应用名称:  " + info.getAppName() + "\n");
-		mtextview6.append("UID:  " + info.getUid() + "\n");
-		mtextview6.append("PID:  " + info.getPid() + "\n");
-		mtextview6.append("进程数量:  " + info.getPidNumber() + "\n");
-		mtextview6.append("GID:  " + info.getGid() + "\n");
+		mtextview4.append("启动时间:  " + info.getLaunTime() + "\n");
+		mtextview4.append("异常时间:  " + info.getExcepTime() + "\n");
+		mtextview4.append("上报时间:  " + info.getUploadTime() + "\n");
+		mtextview4.append("上报次数:  " + info.getUploadNum() + "\n");
+		mtextview4.append("退出时间:  " + info.getExitTime() + "\n");
+		mtextview4.append("运行时间:  " + info.getUseTime() + "\n");
+		mtextview4.append("应用名称:  " + info.getAppName() + "\n");
+		mtextview4.append("UID:  " + info.getUid() + "\n");
+		mtextview4.append("PID:  " + info.getPid() + "\n");
+		mtextview4.append("进程数量:  " + info.getPidNumber() + "\n");
+		mtextview4.append("GID:  " + info.getGid() + "\n");
+
+		// 无线环境信息
+
+		mtextview5.append("无线环境信息:");
+
+		mtextview6.append("运营商:  " + info.getCorporation() + "\n");
+
+		mtextview6.append("LAC_GSM:  " + info.getLAC_GSM() + "\n");
+		mtextview6.append("Cell-ID_GSM:  " + info.getCell_Id_GSM() + "\n\n");
 
 		InfoDatabase infoDatabase = new InfoDatabase(this, "AutoReprt.db", null, 1);// 创建数据库
 																					// //
@@ -153,15 +126,17 @@ public class SecActivity extends Activity
 			for (int i = 0; i < signalInfos.size(); i++)
 			{
 
-				siglist += signalInfos.get(i).getRsrp() + "," + signalInfos.get(i).getRsrq() + ","
-						+ signalInfos.get(i).getRssinr() + "," + signalInfos.get(i).getTxByte() + ","
-						+ signalInfos.get(i).getRxByte() + "," + signalInfos.get(i).getNetType() + ","
-						+ signalInfos.get(i).getPci() + "," + signalInfos.get(i).getCi() + ","
-						+ signalInfos.get(i).getEnodbId() + "," + signalInfos.get(i).getCellId() + ","
-						+ signalInfos.get(i).getTac() + "," + signalInfos.get(i).getTimeStamp()+"|" ;
-//				if((i+1)!=signalInfos.size())
-//				siglist+= "|";
-				
+				siglist += signalInfos.get(i).getTimeStamp() + ", " + signalInfos.get(i).getTxByte() + ", "
+						+ signalInfos.get(i).getRxByte() + ", " + signalInfos.get(i).getRsrp() + ", "
+						+ signalInfos.get(i).getRssinr() + ", " + signalInfos.get(i).getRsrq() + ", "
+						+ signalInfos.get(i).getPci() + ", " + signalInfos.get(i).getCellId() + ", "
+						+ signalInfos.get(i).getEnodbId() + ", " + signalInfos.get(i).getCi() + ", "
+						+ signalInfos.get(i).getTac() + ", " + signalInfos.get(i).getNetType()
+
+						+ "\n\n";
+				// if((i+1)!=signalInfos.size())
+				// siglist+= "|";
+
 			}
 
 		}
